cc26b4ceab6cad1862ac4d7874bf40157868ad95
jlm <j.l.messenger@ieee.org>
2025-02-11 21:57:13 +0000

    Fix copyrights.

--- vh_events.rb
+++ vh_events.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ####
-# Copyright 2016-2022 John Messenger
+# Copyright 2025 John Messenger
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
