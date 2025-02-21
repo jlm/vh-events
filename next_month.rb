3fe9973339b503e21ca2790a746b935ba0886314
jlm <j.l.messenger@ieee.org>
2025-02-21 21:19:23 +0000

    Utility script for making output file names.

--- next_month.rb
+++ next_month.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # Usage: "ruby next_month.rb /tmp/events-%.xlsx" outputs /tmp/events-2025-03.xlsx
+# This is a utility script which might be useful in naming the output file from vh_events.rb.
 require 'date'
 ymstr = (Date.today >> 1).strftime('%Y-%m')
 puts ARGV[0].gsub('%', ymstr)
